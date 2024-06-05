4ae9da36f9ab7ff57525286c911ade7c281994a5
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2024-06-05 19:24:38 +0300

    Update design. Bug fix. Adding more entities.

--- src/main/resources/dml.sql
+++ src/main/resources/dml.sql
@@ -3,23 +3,78 @@
 INSERT INTO category (Name, Description)
 VALUES ('Лялька', 'Різні ляльки для дітей'),
        ('Конструктор', 'Набори конструкторів для творчої гри'),
-       ('Транспорт', 'Іграшкові транспортні засоби, включаючи автомобілі та вантажівки');
+       ('Транспорт', 'Іграшкові транспортні засоби, включаючи автомобілі та вантажівки'),
+       ('М`які іграшки', 'Плюшеві та м`які іграшки для дітей'),
+       ('Настільні ігри', 'Ігри для гри на столі в компанії');
 
 -- Додавання виробників
 INSERT INTO manufacture (Name, Description)
 VALUES ('Mattel', 'Лідируючий виробник іграшок'),
-       ('LEGO', 'Відомий своїми конструкторськими наборами');
+       ('LEGO', 'Відомий своїми конструкторськими наборами'),
+       ('Hasbro', 'Великий виробник іграшок та ігор'),
+       ('Bandai', 'Японський виробник іграшок');
 
 INSERT INTO toy (Name, description, price, category_id, manufacture_id)
-VALUES ('Лялька Барбі', 'Прекрасна лялька Барбі для дітей', 19.99,
+VALUES ('Лялька Барбі з аксесуарами', 'Лялька Барбі з різноманітними аксесуарами', 25.99,
         (SELECT id FROM category WHERE name = 'Лялька'),
         (SELECT id FROM manufacture WHERE name = 'Mattel')),
-       ('Конструктор LEGO City Police Station', 'Побудуйте свою власну поліцейську станцію', 59.99,
+       ('Лялька Барбі Модель', 'Лялька Барбі у модному одязі', 22.99,
+        (SELECT id FROM category WHERE name = 'Лялька'),
+        (SELECT id FROM manufacture WHERE name = 'Mattel')),
+       ('Лялька Барбі Доктор', 'Лялька Барбі у образі лікаря', 27.99,
+        (SELECT id FROM category WHERE name = 'Лялька'),
+        (SELECT id FROM manufacture WHERE name = 'Mattel')),
+
+       ('Конструктор LEGO Star Wars', 'Конструктор для фанатів Зоряних Війн', 69.99,
         (SELECT id FROM category WHERE name = 'Конструктор'),
         (SELECT id FROM manufacture WHERE name = 'LEGO')),
-       ('Набір треків Hot Wheels', 'Цікавий трек для гонок', 29.99,
+       ('Конструктор LEGO Ninjago', 'Захоплюючий конструктор з серії Ninjago', 39.99,
+        (SELECT id FROM category WHERE name = 'Конструктор'),
+        (SELECT id FROM manufacture WHERE name = 'LEGO')),
+       ('Конструктор LEGO Friends', 'Конструктор для дітей з серії Friends', 49.99,
+        (SELECT id FROM category WHERE name = 'Конструктор'),
+        (SELECT id FROM manufacture WHERE name = 'LEGO')),
+
+       ('Трактор Bruder', 'Реалістична модель трактора', 35.99,
         (SELECT id FROM category WHERE name = 'Транспорт'),
-        (SELECT id FROM manufacture WHERE name = 'Mattel'));
+        (SELECT id FROM manufacture WHERE name = 'Mattel')),
+       ('Автомобіль на радіокеруванні', 'Автомобіль з дистанційним управлінням', 45.99,
+        (SELECT id FROM category WHERE name = 'Транспорт'),
+        (SELECT id FROM manufacture WHERE name = 'Mattel')),
+       ('Машинка Hot Wheels', 'Маленька машинка Hot Wheels', 9.99,
+        (SELECT id FROM category WHERE name = 'Транспорт'),
+        (SELECT id FROM manufacture WHERE name = 'Mattel')),
+
+       ('Плюшевий зайчик', 'М`яка іграшка зайчик', 18.99,
+        (SELECT id FROM category WHERE name = 'М`які іграшки'),
+        (SELECT id FROM manufacture WHERE name = 'Hasbro')),
+       ('Плюшевий лев', 'М`яка іграшка лев', 20.99,
+        (SELECT id FROM category WHERE name = 'М`які іграшки'),
+        (SELECT id FROM manufacture WHERE name = 'Hasbro')),
+       ('Плюшевий слон', 'М`яка іграшка слон', 22.99,
+        (SELECT id FROM category WHERE name = 'М`які іграшки'),
+        (SELECT id FROM manufacture WHERE name = 'Hasbro')),
+
+       ('Настільна гра Scrabble', 'Класична настільна гра для створення слів', 19.99,
+        (SELECT id FROM category WHERE name = 'Настільні ігри'),
+        (SELECT id FROM manufacture WHERE name = 'Hasbro')),
+       ('Настільна гра Катан', 'Популярна гра для стратегічного мислення', 29.99,
+        (SELECT id FROM category WHERE name = 'Настільні ігри'),
+        (SELECT id FROM manufacture WHERE name = 'Hasbro')),
+       ('Настільна гра Уно', 'Весела карткова гра для всієї родини', 14.99,
+        (SELECT id FROM category WHERE name = 'Настільні ігри'),
+        (SELECT id FROM manufacture WHERE name = 'Hasbro')),
+
+       ('Конструктор Bandai Dragon Ball', 'Пластиковий модельний набір для складання персонажів Dragon Ball', 34.99,
+        (SELECT id FROM category WHERE name = 'Конструктор'),
+        (SELECT id FROM manufacture WHERE name = 'Bandai')),
+       ('Конструктор Bandai One Piece', 'Пластиковий модельний набір для складання персонажів One Piece', 39.99,
+        (SELECT id FROM category WHERE name = 'Конструктор'),
+        (SELECT id FROM manufacture WHERE name = 'Bandai')),
+       ('Конструктор Bandai Naruto', 'Пластиковий модельний набір для складання персонажів Naruto', 29.99,
+        (SELECT id FROM category WHERE name = 'Конструктор'),
+        (SELECT id FROM manufacture WHERE name = 'Bandai'));
+
 
 -- Додавання користувачів
 INSERT INTO users (Name, Login, Password, Role)
@@ -33,13 +88,13 @@ SELECT Name, 'вул. Головна, 1', '+1234567890' FROM users WHERE role = 
 UNION ALL
 SELECT Name, 'просп. Центральний, 5', '+0987654321' FROM users WHERE role = 'moder';
 
-
-
 -- Додавання секцій складу
 INSERT INTO sections (Name, category_id)
 VALUES ('М`які іграшки', (SELECT id FROM category WHERE name = 'Лялька')),
        ('Конструктори', (SELECT id FROM category WHERE name = 'Конструктор')),
-       ('Іграшкові машинки', (SELECT id FROM category WHERE name = 'Транспорт'));
+       ('Іграшкові машинки', (SELECT id FROM category WHERE name = 'Транспорт')),
+       ('Плюшеві іграшки', (SELECT id FROM category WHERE name = 'М`які іграшки')),
+       ('Настільні ігри', (SELECT id FROM category WHERE name = 'Настільні ігри'));
 
 INSERT INTO users (id, login, password, role, name)
 VALUES ('018f39f9-1826-7cb9-9775-feee72794e6a', 'mike_wilson1', 'password5', 'client',  'mike.wilson'),
